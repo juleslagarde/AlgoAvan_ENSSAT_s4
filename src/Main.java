24c7934f4455fffc043a1e998a3a37620094c8a4
Jules Lagarde <jules.lagarde@gmail.com>
2019-04-12 15:35:22 +0200

    code de Theo Lois

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,73 @@ public class Main {
         return sum;
     }
 
-    private static Set<Ligne> algoEssaisSuccessif() {
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static State algoEssaisSuccessif(Double C) {
         HashSet<Ligne> lignes = new HashSet<>();
-        return lignes;
+        int n=points.length-1;
+        double SD=0;
+        int m=0;
+        double opt=10000;
+        int[] X=new int[n];
+        int[] ChemOpt=new int[n];
+        for (int i=0;i<n;i++){
+            X[i]=0;
+            ChemOpt[i]=0;
+        }
+        X[0]=1;
+        appligbri(0,X,ChemOpt,opt,n,SD,C,m);
+
+
+        System.out.println("Fin Appligbri");
+        for (int j=0;j<n;j++){
+            System.out.println(ChemOpt[j]);
+        }
+        int prec=1;
+        for (int i=2;i<=n;i++){
+            if (ChemOpt[i-1]==1){
+                lignes.add(new Ligne(points[prec],points[i]));
+                prec=i;
+            }
+        }
+        return new State(lignes, opt);
     }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private static void appligbri(int i, int[] X, int[] ChemOpt, double opt, int n, double SD, double C, int m){
+        for (int Xi=n-1;Xi>i;Xi--){
+            X[Xi]=1;
+            m++;
+            SD+=distance(i+1,Xi+1);
+            //if (opt>(m+1)*C+SD){
+            if (Xi==n-1){
+                if (opt>m*C+SD){
+                    opt=(double)m*C+SD;
+                    System.out.println("Nouveau");
+                    for (int j=0;j<n;j++){
+                        ChemOpt[j]=X[j];
+                        System.out.println(ChemOpt[j]);
+                    }
+                    System.out.println("-----");
+                }
+            }else{
+                appligbri(Xi,X,ChemOpt,opt,n,SD,C,m);
+            }
+            //}
+            X[Xi]=0;
+            m--;
+            SD-=distance(i+1,Xi+1);
+        }
+        System.out.println("Renvoi");
+        for (int j=0;j<n;j++){
+            System.out.println(ChemOpt[j]);
+        }
+        System.out.println("-----");
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
     public static State algoDynamique(double C){
         int n = points.length-1;
@@ -64,10 +127,9 @@ public class Main {
         System.out.println();
 
         double c = Double.parseDouble(JOptionPane.showInputDialog("valeur de  C : "));
-        State s = algoDynamique(c);
+        State s = algoEssaisSuccessif(c);
 
         Visu v1 = new Visu(pointSet,s.lignes,"Solution de score "+s.score);
-
     }
 
     static class State{
