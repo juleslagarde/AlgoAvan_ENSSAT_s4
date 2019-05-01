82ada29c9631144f385ff518c9e254c599bb9369
Jules Lagarde <jules.lagarde@gmail.com>
2019-05-01 13:38:01 +0200

    commentaires sur le code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
 
     private static Point[] points;
-    private static int compteur;
 
     public static double distance(int a, int b) {
         double sum = 0;
@@ -15,15 +14,21 @@ public class Main {
         return sum;
     }
 
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * essais successifs
+     * @param C cout de création d'un segment
+     * @return (lignes, cout)
+     */
     private static State algoEssaisSuccessif(Double C) {
         HashSet<Ligne> lignes = new HashSet<>();
-        int n = points.length - 1;
-        double SD = 0;
+        int n = points.length - 1;									//On récupère le nombre de points étudié
+        double SD = 0;												//On initialise les différentes variables
         int m = 0;
         double[] opt = new double[1];
-        opt[0] = 10000;
+        opt[0] = Double.POSITIVE_INFINITY;
         int[] X = new int[n];
         int[] ChemOpt = new int[n];
         for (int i = 0; i < n; i++) {
@@ -31,105 +36,88 @@ public class Main {
             ChemOpt[i] = 0;
         }
         X[0] = 1;
-        appligbri(0, X, ChemOpt, opt, n, SD, C, m);
+        appligbri(0, X, ChemOpt, opt, n, SD, C, m);					//On lance un appel d'appligbri
         int prec = 1;
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {								//On convertit ChemOpt en un ensemble de lignes
             if (ChemOpt[i - 1] == 1) {
                 lignes.add(new Ligne(points[prec], points[i]));
                 prec = i;
             }
         }
-        return new State(lignes, opt[0]);
+        return new State(lignes, opt[0]);							//On renvoie l'ensemble de lignes et son coût
     }
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /**
+     * algo recursif essais successifs
+     *
+     * @param i position dans X à partir de laquel on commence la modification
+     * @param X chemin en cours de construction
+     * @param ChemOpt chemin actuellement optimal
+     * @param opt cout de ChemOpt
+     * @param n taille de X et de ChemOpt
+     * @param SD distance de tous les points inférieurs à i par rapport au chemin X
+     * @param C cout de création d'un segment
+     * @param m nombre de segments
+     */
     private static void appligbri(int i, int[] X, int[] ChemOpt, double[] opt, int n, double SD, double C, int m) {
-        for (int Xi = n - 1; Xi > i; Xi--) {
+        for (int Xi = n - 1; Xi > i; Xi--) {						//On explore tous les points après i
+            //enregistrer(Xi)
             X[Xi] = 1;
             m++;
             SD += distance(i + 1, Xi + 1);
-            if (opt[0] > (m + 1) * C + SD) {
-                //System.out.println("Passage");
-                if (Xi == n - 1) {
-                    if (opt[0] > (double) m * C + SD) {
-                        //System.out.println(opt[0]);
-                        opt[0] = (double) m * C + SD;
-                        //System.out.println("Nouveau");
-                        for (int j = 0; j < n; j++) {
-                            ChemOpt[j] = X[j];
-                            //System.out.println(ChemOpt[j]);
-                        }
-                        //System.out.println("-----");
-                        //System.out.println(opt[0]);
-                        //System.out.println("-----");
+            //
+            if (Xi == n - 1) {		//satisfaisant(Xi)
+                if (opt[0] > (double) m * C + SD) {		//soltrouvée
+                    //optimal
+                    opt[0] = (double) m * C + SD;
+                    for (int j = 0; j < n; j++) {
+                        ChemOpt[j] = X[j];
                     }
-                } else {
+                    //
+                }
+            } else {
+                if (opt[0] > (m + 1) * C + SD) {	//OptEncorePossible
+                    //Le chemin passe maintenant par i puis Xi, et on teste toutes les façons de le compléter
                     appligbri(Xi, X, ChemOpt, opt, n, SD, C, m);
                 }
             }
+            //défaire(Xi)
             X[Xi] = 0;
             m--;
             SD -= distance(i + 1, Xi + 1);
+            //
         }
     }
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    private static State algoEssaisSuccessif2(Double C) {
-//        boolean[] mpoints = new boolean[points.length-1-2];//selected middle points (mpoints[0]==true => points number 2 selected)
-        int max = 1 << points.length -1-2;
-        int pointsI;//bit 0 correspond to points 1, bit 1 to points 2
-        State bestState = new State(null,Double.POSITIVE_INFINITY);
-        for(pointsI = 0; pointsI < max; pointsI++){
-            Set<Ligne> lignes = createLignes(pointsI);
-            //if(bestState.score < lignes.size()*C) break; //elagage, si le nombre de lignes done un score plus grand que le meilleur score
-            //scores
-            double score = lignes.size()*C;
-            for(Ligne l : lignes){
-                score += distance((int)l.getp1().getx(), (int)l.getp2().getx());
-            }
-            if(score<bestState.score){
-                bestState.lignes=lignes;
-                bestState.score=score;
-            }
-        }
-        return bestState;
-    }
-
-    public static Set<Ligne> createLignes(int pointsI){
-        Set<Ligne> lignes = new HashSet<>();
-        int lastPointsSelected = 1;
-        for(int i = 2; i< points.length -1; i++){
-            int mask = 1 << (i - 2);
-            if((pointsI&mask)!=0) {//points selected
-                lignes.add(new Ligne(points[lastPointsSelected], points[i]));
-                lastPointsSelected=i;
-            }
-        }
-        lignes.add(new Ligne(points[lastPointsSelected], points[points.length - 1]));
-        return lignes;
-    }
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
+    /**
+     * algo dynamique
+     * @param C cout de cr&eacute;ation d'un segment
+     * @return (lignes, score)
+     */
     public static State algoDynamique(double C) {
-        int n = points.length - 1;
+        int n = points.length - 1; // le nombre de points
         State[][] states = new State[n + 1][n + 1];
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n-1; i++) { //initialisation de la diagonale b-1-a==0
             HashSet<Ligne> lignes = new HashSet<>();
             lignes.add(new Ligne(points[i+1],points[i+2]));
             states[i+1][i+2]=new State(lignes, C);
         }
+        //iteration sur les diagonales b-delta-a==0
         for(int delta=2; delta<n; delta++){
-//            System.out.println("==========");
-            for (int a = 1, b=a+delta; b <= n; a++, b++) {
+            //iteration le long de la diagonale b-delta-a==0
+            for (int a = 1, b=a+delta; b <= n; a++, b++){
+                //initialisation du chemin optimal avec le chemin direct de a à b
                 HashSet<Ligne> lignes = new HashSet<>();
                 lignes.add(new Ligne(points[a],points[b]));
                 states[a][b] = new State(lignes,distance(a,b)+C);
+                //recherche du score minimal pour le chemin de a à b
                 for (int i = a+1; i < b; i++) {
-//                    System.out.println(a+" "+i+" "+b);
                     State sL = states[a][i];
                     State sR = states[i][b];
                     if(sL.score+sR.score<states[a][b].score){
@@ -140,11 +128,11 @@ public class Main {
                 }
             }
         }
-//        for (State[] d : states) {
-//            System.out.println(Arrays.toString(d));
-//        }
         return states[1][n];
     }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
     public static void main(String[] args) {
@@ -158,19 +146,15 @@ public class Main {
         System.out.println();
 
         double c = Double.parseDouble(JOptionPane.showInputDialog("valeur de  C : "));
-        State s = new State(null, 0);
-//        while (points.length < 19+1) {
-            compteur=0;
-            s = algoDynamique(c);
-            System.out.println((points.length-1) +"\t"+compteur+"\t"+s.score);
-//            points = Arrays.copyOf(points, points.length+1);
-//            points[points.length-1] = new Point(points.length-1, rand.nextDouble()*3);
-//            pointSet.add(points[points.length-1]);
-//        }
-//        State s = algoEssaisSuccessif2(c);
-
-        Visu v1 = new Visu(pointSet, s.lignes, "Solution de score " + s.score);
+        State s = algoEssaisSuccessif(c);
+        Visu v1 = new Visu(pointSet, s.lignes, "Solution de score avec algorithme successif " + s.score);
+        State s1 = algoDynamique(c);
+        Visu v2 = new Visu(pointSet, s1.lignes, "Solution de score avec algorithme dynamique " + s1.score);
     }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
     static class State {
         public Set<Ligne> lignes;
